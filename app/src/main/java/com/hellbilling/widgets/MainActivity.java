69840ac6c6fc8bc01cd98ee807880d5ade68d274
culakm <culak.martin@gmail.com>
2015-01-13 23:38:19 +0100

    pridane prepinace

--- app/src/main/java/com/hellbilling/widgets/MainActivity.java
+++ app/src/main/java/com/hellbilling/widgets/MainActivity.java
@@ -5,43 +5,76 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Button;
 
 import java.util.Date;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements
+        CompoundButton.OnCheckedChangeListener {
 
     int i = 0;
+    CheckBox wCheckBoxMoj;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // we vant to monitor status of the check box
+        // Swutch supports the same behavior
+        wCheckBoxMoj=(CheckBox)findViewById(R.id.moj_check_box_id);
+        wCheckBoxMoj.setOnCheckedChangeListener(this);
+
     }
 
     public void buttonSendsMessage(View view) {
-        // Declaration: text widget
-        TextView wText;
         // Declaration: button, from the view
         Button wButton = (Button)view;
-        // Search button in layout
-        wText=(TextView)findViewById(R.id.moj_text_view_id);
+        // Search widgets in layout
+        TextView wText=(TextView)findViewById(R.id.moj_text_view_id);
+        EditText wEditText=(EditText)findViewById(R.id.moj_edit_text_id);
+        wCheckBoxMoj=(CheckBox)findViewById(R.id.moj_check_box_id);
+        Switch wSwitch=(Switch)findViewById(R.id.moj_switch_id);
+        RadioGroup wRadioGroup=(RadioGroup)findViewById(R.id.moj_redio_group_id);
         // Change text widget's string
-        wText.setText(new Date().toString() + "\nbutton text: " + wButton.getText().toString() + "\ncounter: " + i++);
+        wText.setText(
+            new Date().toString() + "\n" +
+            "button text: " + wButton.getText().toString() + "\n" +
+            "counter: " + i++ + "\n" +
+            "editovany text: " + wEditText.getText() + "\n" +
+            "check box status: " + wCheckBoxMoj.isChecked() + "\n" +
+            "switch status: " + wSwitch.isChecked() + "\n" +
+            "radio status: " + wRadioGroup.getCheckedRadioButtonId() + "\n" +
+            "");
 
     }
 
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked){
+        if(isChecked){
+            wCheckBoxMoj.setText("check box checked");
+        }
+        else {
+            wCheckBoxMoj.setText("check box NOT checked");
+        }
+    }
     public void buttonSendsMessage2(View view) {
         // Declaration: text widget
         TextView wText;
+        EditText wEditText;
         // Declaration: button, from the view
         //Button wButton = (Button)view;
         // Search button in layout
         wText=(TextView)findViewById(R.id.moj_text_view_id);
+        wEditText=(EditText)findViewById(R.id.moj_edit_text_id);
         // Change text widget's string
-        wText.setText(new Date().toString() + "\nbutton text: "+ "\ncounter: " + i++);
+        wText.setText("Text written: " + wEditText.getText() + "\ncounter: " + i++);
 
     }
     @Override
